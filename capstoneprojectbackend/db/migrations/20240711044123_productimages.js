40a741cf05610e6a63b26e11a2dcfa929ae35aa0
nelg62 <glen.harding.nz@gmail.com>
2024-07-11 17:11:07 +1200

    starting knex product image and seed

---
+++ capstoneprojectbackend/db/migrations/20240711044123_productimages.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("productimages", (table) => {
+    table.increments("id").primary();
+    table.string("image");
+    table.foreign("productId").references("Product.id");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
