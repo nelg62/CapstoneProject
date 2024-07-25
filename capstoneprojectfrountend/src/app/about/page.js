7cfd6e81c3c167682cf97743577d443fc7768dc0
nelg62 <glen.harding.nz@gmail.com>
2024-07-25 20:34:22 +1200

    merge error fix branchabout page tidying

--- capstoneprojectfrountend/src/app/about/page.js
+++ capstoneprojectfrountend/src/app/about/page.js
@@ -1,97 +1,141 @@
 "use client";
 
+import { Divider, List, ListItem, Paper, Typography } from "@mui/material";
 import { Box, Container, width } from "@mui/system";
+import Link from "next/link";
 
 const AboutPage = () => {
   return (
     <>
-      <h1>Welcome to the About Page</h1>
+      <Typography variant="h1" textAlign={"center"} gutterBottom>
+        Welcome to the About Page
+      </Typography>
+      <Divider sx={{ marginBottom: "10px" }}></Divider>
       <Container>
-        <h2>Frountend</h2>
+        <Paper>
+          <Typography variant="h2" textAlign={"center"} gutterBottom>
+            Frountend
+          </Typography>
 
-        <h4>Figma Planing</h4>
-        <Box
-          component="img"
-          alt="FrountEnd Diagram Plan"
-          src="Capstone project.png"
-          sx={{ width: "100%" }}
-        ></Box>
-        <ul>
-          <li>React</li>
-          <li>Next.js</li>
-          <li>MUI</li>
-          <li>Axios</li>
-        </ul>
+          <Typography variant="h4" textAlign={"center"}>
+            Figma Planing
+          </Typography>
+          <Box
+            component="img"
+            alt="FrountEnd Diagram Plan"
+            src="Capstone project.png"
+            sx={{ width: "100%" }}
+          ></Box>
+          <List>
+            <ListItem>React</ListItem>
+            <ListItem>Next.js</ListItem>
+            <ListItem>MUI</ListItem>
+            <ListItem>Axios</ListItem>
+          </List>
+        </Paper>
+        <Divider sx={{ marginBottom: "10px" }}></Divider>
 
-        <h2>Backend</h2>
+        <Paper>
+          <Typography variant="h2" textAlign={"center"} gutterBottom>
+            Backend
+          </Typography>
 
-        <a
-          href="http://localhost:8081/api-docs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Swagger File
-        </a>
+          <Link
+            href="http://localhost:8081/api-docs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Swagger File
+          </Link>
 
-        <ul>
-          <li>Express</li>
-          <li>Knex</li>
-          <li>Axios</li>
-          <li>Bcrypt</li>
-          <li>Cors</li>
-          <li>Jsonwebtoken</li>
-        </ul>
+          <List>
+            <ListItem>Express</ListItem>
+            <ListItem>Knex</ListItem>
+            <ListItem>Axios</ListItem>
+            <ListItem>Bcrypt</ListItem>
+            <ListItem>Cors</ListItem>
+            <ListItem>Jsonwebtoken</ListItem>
+          </List>
+        </Paper>
 
-        <h2>Container</h2>
-        <ul>
-          <li>Docker</li>
-        </ul>
+        <Divider sx={{ marginBottom: "10px" }}></Divider>
 
-        <h2>Database</h2>
+        <Paper>
+          <Typography variant="h2" textAlign={"center"} gutterBottom>
+            Container
+          </Typography>
+          <List>
+            <ListItem>Docker</ListItem>
+          </List>
+        </Paper>
 
-        <h4>Database Diagram</h4>
-        <Box
-          component="img"
-          alt="FrountEnd Diagram Plan"
-          src="Capstone Database Diagram.png"
-          sx={{ width: "100%" }}
-        ></Box>
-        <ul>
-          <li>MySQL</li>
-        </ul>
+        <Divider sx={{ marginBottom: "10px" }}></Divider>
 
-        <h1>Future expanions</h1>
-        <ul>
-          <li>
-            Learn and Change to typescript if I dont just start a new project
-          </li>
-          <li>
-            Add Sequelize with Knex to make backend and database connetions
-            smoother
-          </li>
-          <li>
-            Add realtime support chat feature e.g from the socket.io lab we had
-            done in class
-          </li>
-          <li>
-            Add user page to check and change details of logged in user and
-            check ordered items
-          </li>
-          <li>
-            Add difirent tiers of login such as admin logins who can add remove
-            and edit products
-          </li>
-          <li>
-            Add checkout form to collect postage data and payment info after
-            purchase
-          </li>
-          <li>
-            after purchacing update amount of products by removing from amount
-            available
-          </li>
-          <li>add abbility to add comments and rate products</li>
-          <li>make ratings on products correct and not from dummy data info</li>
-        </ul>
+        <Paper>
+          <Typography variant="h2" textAlign={"center"} gutterBottom>
+            Database
+          </Typography>
+
+          <Typography variant="h4" textAlign={"center"}>
+            Database Diagram
+          </Typography>
+          <Box
+            component="img"
+            alt="FrountEnd Diagram Plan"
+            src="Capstone Database Diagram.png"
+            sx={{ width: "100%" }}
+          ></Box>
+          <List>
+            <ListItem>MySQL</ListItem>
+          </List>
+        </Paper>
+
+        <Divider sx={{ marginBottom: "10px" }}></Divider>
+
+        <Paper>
+          <Typography variant="h1" textAlign={"center"} gutterBottom>
+            Future expanions
+          </Typography>
+          <List>
+            <ListItem>
+              Learn and Change to typescript if I dont just start a new project
+            </ListItem>
+            <ListItem>
+              Add Sequelize with Knex to make backend and database connetions
+              smoother
+            </ListItem>
+            <ListItem>
+              Add realtime support chat feature e.g from the socket.io lab we
+              had done in class
+            </ListItem>
+            <ListItem>
+              Add user page to check and change details of logged in user and
+              check ordered items
+            </ListItem>
+            <ListItem>
+              Add difirent tiers of login such as admin logins who can add
+              remove and edit products
+            </ListItem>
+            <ListItem>
+              Add checkout form to collect postage data and payment info after
+              purchase
+            </ListItem>
+            <ListItem>
+              after purchacing update amount of products by removing from amount
+              available
+            </ListItem>
+            <ListItem>add abbility to add comments and rate products</ListItem>
+            <ListItem>
+              make ratings on products correct and not from dummy data info
+            </ListItem>
+            <ListItem>
+              Save products to local storage or cachce and only fetch from
+              server if needed to save on loading times
+            </ListItem>
+            <ListItem>Dokerize or host in azure cloud</ListItem>
+          </List>
+        </Paper>
+        <Divider sx={{ marginBottom: "10px" }}></Divider>
       </Container>
     </>
   );
