8517c0647f1786a38fe12f72ae0f94fa9ba54163
nelg62 <glen.harding.nz@gmail.com>
2024-08-13 12:24:47 +1200

    migrate changes for cloud

--- capstoneprojectbackend/knexfile.js
+++ capstoneprojectbackend/knexfile.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+console.log("process.env.CLOUD_DB_HOST:", process.env.CLOUD_DB_HOST);
 
 console.log("process.env", process.env.DB_HOST);
 /**
@@ -6,12 +7,13 @@ console.log("process.env", process.env.DB_HOST);
  */
 module.exports = {
   development: {
-    client: "mysql",
+    client: "mysql2",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.CLOUD_DB_HOST,
+      user: process.env.CLOUD_DB_USER,
+      password: process.env.CLOUD_DB_PASSWORD,
+      database: process.env.CLOUD_DB_NAME,
+      port: process.env.CLOUD_DB_PORT,
     },
     migrations: {
       directory: "./db/migrations",
