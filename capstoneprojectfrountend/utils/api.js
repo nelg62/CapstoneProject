91d717e198e1f9704379cd5f90f015c1258611e5
nelg62 <glen.harding.nz@gmail.com>
2024-08-13 12:49:56 +1200

    change api to point to cloud backend

--- capstoneprojectfrountend/utils/api.js
+++ capstoneprojectfrountend/utils/api.js
@@ -1,13 +1,19 @@
 // const ProductApi = "https://dummyjson.com/products";
-const ProductApi = "http://localhost:8081/api/products";
+// const ProductApi = "http://localhost:8081/api/products";
+const ProductApi = "https://capstoneproject-gpwn.onrender.com/api/products";
 
 // const MultipleProductsApi = "https://dummyjson.com/products?limit=10";
-const MultipleProductsApi = "http://localhost:8081/api/products"; // madie add limit to this
+// const MultipleProductsApi = "http://localhost:8081/api/products"; // madie add limit to this
+const MultipleProductsApi =
+  "https://capstoneproject-gpwn.onrender.com/api/products";
 
-const CartApi = "http://localhost:8081/api/cart";
+// const CartApi = "http://localhost:8081/api/cart";
+const CartApi = "https://capstoneproject-gpwn.onrender.com/api/cart";
 
-const UserApi = "http://localhost:8081/api/users";
+// const UserApi = "http://localhost:8081/api/users";
+const UserApi = "https://capstoneproject-gpwn.onrender.com/api/users";
 
-const OrdersApi = "http://localhost:8081/api/orders";
+// const OrdersApi = "http://localhost:8081/api/orders";
+const OrdersApi = "https://capstoneproject-gpwn.onrender.com/api/orders";
 
 export { ProductApi, MultipleProductsApi, CartApi, UserApi, OrdersApi };
